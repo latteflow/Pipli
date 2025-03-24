27b61795aba676b4bc0a373678421a1266e9a420
Satish Gaurav <deepsatflow@gmail.com>
2025-03-24 22:32:41 +0530

    add new screen and try to debug bluetooth

--- app/Pipli/app/(tabs)/_layout.tsx
+++ app/Pipli/app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="connect"
+        options={{
+          title: 'Connect',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="chevron.right" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
